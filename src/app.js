2a0ceececaac09a094e828d66ebcc970038774cf
GabrielFJanu <gabrielfreitasj2004@gmail.com>
2026-05-06 22:57:02 -0300

    errorMiddleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cursoRoutes from './routes/cursoRoutes.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import { handleGlobalErrors } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(express.json());
 
 app.use('/api/cursos', cursoRoutes);
 app.use('/api/usuarios', usuarioRoutes);
+
+app.use(handleGlobalErrors);
 
 export default app;
