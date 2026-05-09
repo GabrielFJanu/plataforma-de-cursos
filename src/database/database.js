76dc671c344793cc181baa428e42c99e075174bb
GabrielFJanu <gabrielfreitasj2004@gmail.com>
2026-05-08 22:54:39 -0300

    exclusão de redundancia db.read()

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,9 @@
 import { Low } from 'lowdb';
 import { JSONFile } from 'lowdb/node';
+
 const defaultData = { usuarios: [], cursos: [] };
+
 const adapter = new JSONFile('db.json');
 const db = new Low(adapter, defaultData);
-await db.read();
+
 export default db;
