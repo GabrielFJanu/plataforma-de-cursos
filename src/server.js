e7098289593752db4781934f1d535452b8fce394
GabrielFJanu <gabrielfreitasj2004@gmail.com>
2026-06-13 15:51:24 -0300

    mensagem de deploy

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ await connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando`);
 });
